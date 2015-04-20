04ff37e16b9269366123673294a6c149b086d967
Victor A. Ying <ying.victor@gmail.com>
2015-04-19 20:46:47 -0400

    Changed timing of TX test.
    Changed length of pings to 2 ms. Also made cosmetic changes.

--- arduino/test/tx_test/tx_test.ino
+++ arduino/test/tx_test/tx_test.ino
@@ -3,18 +3,20 @@
 // transmitter stations sending out pings in turn.
 
 enum {
-  tx_pin_1 = 3,
-  tx_pin_2 = 11,
+  TX_PIN_1 = 3,
+  TX_PIN_2 = 11,
+  FREQ = 25000u,  // Hz
+  DURATION = 2u,  // ms
 };
 
 void setup() {
-  pinMode(tx_pin_1, OUTPUT);
-  pinMode(tx_pin_2, OUTPUT);
+  pinMode(TX_PIN_1, OUTPUT);
+  pinMode(TX_PIN_2, OUTPUT);
 }
 
 void loop() {
-  tone(tx_pin_1, 25000u, 1u);
+  tone(TX_PIN_1, FREQ, DURATION);
   delay(100u);
-  tone(tx_pin_2, 25000u, 1u);
+  tone(TX_PIN_2, FREQ, DURATION);
   delay(1000u);
 }

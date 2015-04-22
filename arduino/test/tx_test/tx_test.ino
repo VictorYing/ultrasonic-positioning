851fc5ff90b3b84a70ab679e3ecac772f5fa3491
Victor A. Ying <ying.victor@gmail.com>
2015-04-22 18:32:42 -0400

    Make each transmitter controlled by two pins.
    By turning them on and off out of phase, we effectively get a voltage
    range of +5V to -5V accross the transistor, quadrupling the power.
    
    Unfortunately, the Arduino only can generate one square wave
    automatically at a time, so to generate two square waves simulatneously
    at high speed/timing accuracy requires low level port trickery.

--- arduino/test/tx_test/tx_test.ino
+++ arduino/test/tx_test/tx_test.ino
@@ -1,22 +1,63 @@
-// One Arduino controls transmitters on pins 3 and 11, alternately
-// sending out pings from the two of them, simulating two synchronized
+// One Arduino controls two transmitters, alternately sending
+// out pings from the two of them, simulating two synchronized
 // transmitter stations sending out pings in turn.
 
+#define TX_1_PORT PORTD
+#define TX_2_PORT PORTB
+
 enum {
-  TX_PIN_1 = 3,
-  TX_PIN_2 = 11,
+  TX_1_PIN_1 = 2,
+  TX_1_PIN_2 = 3,
+  TX_2_PIN_1 = 10,
+  TX_2_PIN_2 = 11,
   FREQ = 25000u,  // Hz
-  DURATION = 5u,  // ms
+  PERIOD = 1000000u / FREQ,  // μs
+  HALF_PERIOD = PERIOD / 2u,  // μs
+  DURATION = 5000u,  // μs
+  SPACING = 100000u,  // μs
+  CYCLE = 1000000u,  // μs
 };
 
 void setup() {
-  pinMode(TX_PIN_1, OUTPUT);
-  pinMode(TX_PIN_2, OUTPUT);
+  pinMode(TX_1_PIN_1, OUTPUT);
+  pinMode(TX_1_PIN_2, OUTPUT);
+  pinMode(TX_2_PIN_1, OUTPUT);
+  pinMode(TX_2_PIN_2, OUTPUT);
 }
 
 void loop() {
-  tone(TX_PIN_1, FREQ, DURATION);
-  delay(100u);
-  tone(TX_PIN_2, FREQ, DURATION);
-  delay(1000u);
+  unsigned long beginning = micros();
+
+  // Send out ping from transmitter 1
+  unsigned long time = micros() - beginning;
+  unsigned long next = time;
+  while (time < DURATION) {
+    TX_1_PORT = TX_1_PORT & (~(1 << (TX_1_PIN_2 % 8))) | (1 << (TX_1_PIN_1 % 8));
+    next += HALF_PERIOD;
+    while (micros() - beginning < next)
+      ;
+    TX_1_PORT = TX_1_PORT & (~(1 << (TX_1_PIN_1 % 8))) | (1 << (TX_1_PIN_2 % 8));
+    next += HALF_PERIOD;
+    while ((time = micros() - beginning) < next)
+      ;
+  }
+
+  // Send out a ping from transmitter 2
+  while ((time = micros() - beginning) < SPACING)
+    ;
+  next = time;
+  while (time < SPACING + DURATION) {
+    TX_2_PORT = TX_2_PORT & (~(1 << (TX_2_PIN_2 % 8))) | (1 << (TX_2_PIN_1 % 8));
+    next += HALF_PERIOD;
+    while (micros() - beginning < next)
+      ;
+    TX_2_PORT = TX_2_PORT & (~(1 << (TX_2_PIN_1 % 8))) | (1 << (TX_2_PIN_2 % 8));
+    next += HALF_PERIOD;
+    while ((time = micros() - beginning) < next)
+      ;
+  }
+
+  // Wait for end of cycle
+  while (micros() - beginning < CYCLE)
+    ;
 }

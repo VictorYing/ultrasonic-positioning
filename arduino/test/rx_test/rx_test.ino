a5aa0fe1c0c6e79dc663ec831977c3e1d6f92b9b
Victor A. Ying <ying.victor@gmail.com>
2015-04-23 15:01:15 -0400

    Added rx_test.

---
+++ arduino/test/rx_test/rx_test.ino
@@ -0,0 +1,28 @@
+// Mimics the receiver board, sending a repeating series of four pings
+// as if from four transmitter stations to the PSoC, for the PSoC to
+// attempt position calculations.
+
+enum {
+  RX_PIN = 11,
+  NUM_TRANSMITTERS = 4u,
+  DURATION = 5000u,  // μs
+};
+
+unsigned const long SPACINGS[4] = {100000u, 100000u, 100000u, 700000u};
+
+void setup() {
+  pinMode(RX_PIN, OUTPUT);
+}
+
+void loop() {
+  static unsigned i = 0;
+  digitalWrite(RX_PIN, HIGH);
+  unsigned long beginning = micros();
+  while (micros() - beginning < DURATION)
+    ;
+  digitalWrite(RX_PIN, LOW);
+  unsigned long spacing = SPACINGS[i];
+  i = (i + 1) % NUM_TRANSMITTERS;
+  while (micros() - beginning < spacing)
+    ;
+}
